0ccf2d9700a68b5be17a5839ae5142554af97e52
kyonori1 <reustache2019@bklawtech.com>
2018-02-26 23:26:04 +0000

    hello

--- src/WeekPage.js
+++ src/WeekPage.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import Header from './Header'
+import getForecast from './WeatherData.js'
 
 class WeekPage extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <Header title='5 Day Forecast'/>
-      </div>
-    )
-  }
-}
+    constructor(props) {
+      super(props);
+      this.state = {
+        forcast: []
+      };
+    }
 
-export default WeekPage
+    componentDidMount() {
+      var self = this;
+      getForecast(function(data) {
+        self.setState({ forcast: data.list });
+
+    });
+    }
+    render() {
+      return (
+        <div className='App'>
+      {this.state.forecast.map((hour) =>
+      <div>
+        <div>{hour.dt_txt}</div>
+          <img src={`https://openweathermap.org/img/w/${hour.weather[0].icon}.png`} alt="Weather Icon"/>
+          <div>
+           <span>{hour.main.temp_max}°</span>
+            <span>{hour.main.temp_min}°</span>
+          </div>
+      </div>
+      )}
+     </div>
+      );
+    }
+
+    export default WeekPage
